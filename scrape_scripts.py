d64ddaca6d7e427783888819439b984d5d90f8e8
Jenna <jennamorabito25@gmail.com>
2021-11-14 19:51:50 -0800

    look ma, a script

---
+++ scrape_scripts.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://imsdb.com/scripts/12-and-Holding.html"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+script = soup.find('pre')
+
+print(script.prettify())
